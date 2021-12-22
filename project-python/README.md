c3ec1aecda13b4a8dda2a41abc714c9a6cd9f772
zanevakalonika <zaneva0007@gmail.com>
2021-12-21 21:31:22 -0800

    first commit

---
+++ project-python/README.md
@@ -0,0 +1,29 @@
+<!-- Title -->
+# **Interactive Calculator**
+
+<!-- Img Thumbnail -->
+<p align="center">
+    <img style="border-radius:5%;" class="img-thumbnail" src="calculator.gif"/>
+</p>
+
+<!-- Description of Img Thumbnail -->
+<p>
+    👋 Hi, this is my first python project, which is an interactive calculator which can help you calculate groceries. This calculator can be considered complicated because you have to copy and paste the code from my github and also have to run it in the code editor, but because this is a fun project so it's just for fun. Enjoy with my first python project
+</p>
+
+<!-- Procedur to Run the Program -->
+## 👇 How to Run Interactive Calculator Program
+
+- **Steps in Github Page**
+    - First, open the repository named project.python 
+    - Second, copy the code from the repository with the name countable.py
+- **Steps in Code Editor**
+    - First, open your favorite code editor
+    - Second, create a new file with the same name which is countable.py
+    - Third, paste the code from the repository with the name countable.py
+    - Fourth, run the project by pressing the play button at the top right of the code editor (Visual Studio Code)
+<!-- Thank You Card and Social Media -->
+### 🙏 **Thank you for viewing my project**
+[![facebook](https://img.shields.io/badge/Facebook-5279E0?style=for-the-badge&logo=facebook&logoColor=white)](https://www.facebook.com/zaneva.handoko/)
+[![twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/zanevakalonika)
+[![instagram](https://img.shields.io/badge/Instagram-B983FF?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/zanevakalonika/)
---
+++ project-python/countable.py
@@ -0,0 +1,53 @@
+# Input First Question
+input_things = input('Anda ingin membeli apa ?')
+input_count = input('Berapa harga ' + str(input_things) + ' yang ingin Anda beli ?')
+input_jumlah = input('Berapa jumlah ' + str(input_things) + ' yang ingin Anda beli ?')
+# Variable Total
+total_price = int(input_count) * int(input_jumlah)
+# Respon Total
+print('Total harga belanjaan ' + str(input_jumlah) + ' ' + str(input_things) + ' yang akan Anda beli adalah ' + str(total_price))
+# Input Second Question
+input_budget = input('Berapa uang budget yang Anda punya ?')
+# Kondisi
+if int(input_budget) > int(total_price):
+    print('Anda telah membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+    print('Uang Anda tinggal ' + str(int(input_budget) - int(total_price)))
+elif int(input_budget) == int(total_price):
+    print('Jika Anda membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+    print('Uang Anda tinggal ' + str(int(total_price) - int(input_budget))) 
+    input_question = input('Apakah Anda tetap ingin membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' ?')
+    if input_question == 'yes' or input_question == 'ya':
+        print('Anda telah membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+        print('Uang Anda tinggal ' + str(int(total_price) - int(input_budget))) 
+    if input_question == 'no' or input_question == 'tidak':
+        print('Anda tidak jadi membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+        print('Uang Anda tetap berjumlah ' + str(input_budget))
+if int(input_budget) < int(total_price):
+    print('Anda tidak dapat membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+    jumlah_kekurangan = int(total_price) - int(input_budget)
+    print('Anda dapat membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price) + ' dengan menambahkan uang budget sejumlah ' + str(jumlah_kekurangan))
+    input_questyesno = input('Apakah Anda ingin menambahkan uang budget ?')
+    if input_questyesno == 'yes' or input_questyesno == 'ya':
+        input_answer = input('Berapa uang budget yang ingin Anda tambahkan ?')
+        if int(input_answer) > int(jumlah_kekurangan):
+            print('Uang Anda telah cukup untuk membeli ' + str(input_jumlah) + ' ' + str(input_things))
+            print('Anda telah membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+            print('Uang Anda tinggal ' + str(int(total_price) - int(input_budget))) 
+        elif int(input_answer) == int(jumlah_kekurangan):
+            total_budget = int(input_answer) + int(input_budget)
+            sisa_uang = int(total_price) - int(total_budget)
+            print('Jika Anda membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+            print('Uang Anda tinggal ' + str(sisa_uang))
+            input_question = input('Apakah Anda tetap ingin membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' ?')
+            if input_question == 'yes' or input_question == 'ya':
+                print('Anda telah membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+                print('Uang Anda tinggal ' + str(sisa_uang)) 
+            if input_question == 'no' or input_question == 'tidak':
+                print('Anda tidak jadi membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+                print('Uang Anda tetap berjumlah ' + str(total_budget))
+        if int(input_answer) < int(jumlah_kekurangan):
+            print('Anda tidak dapat membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+            print('Uang Anda tetap berjumlah ' + str(total_budget))
+    if input_questyesno == 'no' or input_questyesno == 'tidak':
+        print('Anda tidak jadi membeli ' + str(input_jumlah) + ' ' + str(input_things) + ' seharga ' + str(total_price))
+        print('Uang Anda tetap berjumlah ' + str(input_budget)) 
