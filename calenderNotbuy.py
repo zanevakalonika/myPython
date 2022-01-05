b6ed245e41be34f8bf7b55f61ef68f64f337fd0f
Zaneva Kalonika Handoko <88816683+zanevakalonika@users.noreply.github.com>
2022-01-04 17:05:17 -0800

    Update calenderNotbuy.py

--- calenderNotbuy.py
+++ calenderNotbuy.py
@@ -1,14 +1,12 @@
 # import kalender
 import calendar
 
-# variable untuk perulangan while
-calendar_try = 1
 # username input
 input_user_name = input('siapakah anda :')
 # print data username input
 print('hallo,' , input_user_name)
 # perulangan while
-while calendar_try != 0:
+while True:
     input_calendar = input('''jawab dengan cara ini (tahun,bulan)\ningin kalender tahun dan bulan berapa :''')
     calendar_output = calendar.month(int(input_calendar[0:4]),int(input_calendar[5:]))
     print(calendar_output)
